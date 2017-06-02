cf386bb870f6d8133c1fb45a519d803878e5797d
bitshift <bitshift@posteo.org>
2017-06-03 00:42:56 +0200

    Sketch for mapping a texture onto a mesh. Not working.

--- src/SkinSource.cpp
+++ src/SkinSource.cpp
@@ -3,13 +3,13 @@
 #include <midi.h>
 #include <algorithms.h>
 
-//--------------------------------------------------------------
+//-----------------------------------------------------------------------------
 
 SkinSource::SkinSource(midi::in& midi_in)
 	: midi_in_(midi_in)
 {
 	name = "Skin";
-	allocate(450, 300);
+	allocate(width_, height_);
 }
 
 //-----------------------------------------------------------------------------
@@ -17,12 +17,11 @@ SkinSource::SkinSource(midi::in& midi_in)
 void SkinSource::setup()
 {
 	ofImage image;
-    image.load("assets/skin_tex_01_unsharpen.jpg");
-	image_.setFromPixels(image.getPixels());
-	image.load("assets/skin_tex_02_unsharpen.jpg");
-	image2_.setFromPixels(image.getPixels());
+    image_.load("assets/skin_tex_01_unsharpen.jpg");
+	image2_.load("assets/skin_tex_03.jpg");
 
-	overlay_px_.allocate(image_.getWidth(), image_.getHeight(), OF_IMAGE_COLOR_ALPHA);
+	overlay_px_.allocate(image_.getWidth(), image_.getHeight(), 
+					     OF_IMAGE_COLOR_ALPHA);
 	
 	// RED
 	midi_in_.add_trigger(midi::trigger_t{ 100, [this](const size_t value){ 
@@ -44,9 +43,73 @@ void SkinSource::setup()
 	midi_in_.add_trigger(midi::trigger_t{ 117, [this](const size_t value){ 
 		image::set_channel(overlay_px_, 3, value * 2);
 	}});
+
+	//Set up vertices
+	for (int y=0; y<H; y++) 
+	{
+		for (int x=0; x<W; x++) 
+		{ 
+			// adding texure coordinates allows us to bind textures to it later 
+			// --> this could be made into a function so that 
+			// textures can be swapped / updated
+			mesh_.addVertex(ofPoint((x - W/2) * meshSize_, 
+									(y - H/2) * meshSize_, 0 )); 
+			mesh_.addTexCoord(ofPoint(x * (width_ / W), y * (height_ / H)));
+			mesh_.addColor(ofColor(255, 255, 255));
+		}
+	}
+
+	//Set up triangles' indices
+	for (int y=0; y<H-1; y++) 
+	{
+		for (int x=0; x<W-1; x++) 
+		{
+			int i1 = x + W * y;
+			int i2 = x+1 + W * y;
+			int i3 = x + W * (y+1);
+			int i4 = x+1 + W * (y+1);
+			mesh_.addTriangle( i1, i2, i3 );
+			mesh_.addTriangle( i2, i4, i3 );
+		}
+	}
 }
 
-//--------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
+void SkinSource::update()
+{
+	//convert fbo to ofImage format
+	fbo->readToPixels(fbo_px_);
+	image_.setFromPixels(fbo_px_);
+
+	//Change vertices
+	for (int y=0; y<H; y++) 
+	{
+		for (int x=0; x<W; x++) 
+		{
+
+			//Vertex index
+			int i = x + W * y;
+			ofPoint p = mesh_.getVertex( i );
+
+			float scaleX = width_ / W;
+			float scaleY = height_ / H;
+
+			// get brightness
+			int index = ((x * scaleX) + width_ * (y * scaleY)) * 4; // FBO has four components (including Alpha)
+			int brightness = fbo_px_[index] / 4; // 4 is an arbitrary scalar to reduce the amount of distortion
+
+			//Change z-coordinate of vertex
+			p.z = brightness; // ofNoise(x * 0.05, y * 0.05, ofGetElapsedTimef() * 0.5) * 100;
+			mesh_.setVertex( i, p );
+
+			//Change color of vertex
+			mesh_.setColor(i , ofColor(255, 255, 255));
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
 
 void SkinSource::draw()
 {
@@ -55,6 +118,18 @@ void SkinSource::draw()
 
 	overlay_.setFromPixels(overlay_px_);
 
-	image_.draw(0, 0);
-	overlay_.draw(0, 0);
+	// image_.draw(0, 0);
+	// overlay_.draw(0, 0);
+
+	ofPushMatrix(); //Store the coordinate system
+
+	image_.bind();
+
+	//Move the coordinate center to screen's center
+	// ofTranslate(width_ / 2, height_ / 2, 0);
+	// mesh_.drawWireframe();
+	mesh_.draw();
+
+	image_.unbind();
+	ofPopMatrix(); //Restore the coordinate system
 }
--- src/SkinSource.h
+++ src/SkinSource.h
@@ -14,14 +14,27 @@ public:
 
 	void setup();
 	void draw();
+	void update();
 
 private:
-	ofxCvColorImage image_;
-	ofxCvColorImage image2_;
+	ofImage image_;
+	ofImage image2_;
+
+	ofPixels fbo_px_;
+
 	ofImage overlay_;
 	ofPixels overlay_px_;
+	ofMesh mesh_;
 
 	midi::in& midi_in_;
 
 	ofParameter<size_t> saturation_;
+
+	const size_t height_ = 1024;
+	const size_t width_ = 1024;
+	const size_t meshSize_ = 6;
+
+	int W = 200;
+	int H = 100;
+	
 };
